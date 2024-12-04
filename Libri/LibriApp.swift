500179d5332e49701fb573d0a90afa1892373157
RaspyGray <tony031@hotmail.it>
2024-12-04 16:37:49 +0100

    Initial Commit

---
+++ Libri/LibriApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibriApp.swift
+//  Libri
+//
+//  Created by Antonio Nardone on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LibriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
